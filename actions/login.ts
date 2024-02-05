ef9418555850000d73852f4b57f1c0774efc2148
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2024-02-05 17:26:44 +0200

    init db & prisma

--- actions/login.ts
+++ actions/login.ts
@@ -2,8 +2,6 @@
 
 import * as z from "zod";
 
-import { revalidatePath, revalidateTag } from "next/cache";
-
 import { LoginSchema } from "@/schemas";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
--- actions/register.ts
+++ actions/register.ts
@@ -1,12 +1,11 @@
 "use server";
 
 import * as z from "zod";
-
 import { RegisterSchema } from "@/schemas";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
-  console.log(validatedFields);
+
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
