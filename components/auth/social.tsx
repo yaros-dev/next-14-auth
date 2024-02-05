5121693f857f12cfce40562b85ef59dd60d77210
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2024-02-05 15:20:30 +0200

    add register form

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -6,13 +6,26 @@ import { Button } from "../ui/button";
 
 const Social = () => {
   return (
-    <div className="flex items-center w-full gap-x-2">
-      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
-        <FcGoogle className="h-5 w-5" />
-      </Button>
-      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
-        <FaGithub className="h-5 w-5" />
-      </Button>
+    <div className="w-full flex flex-col gap-y-2">
+      <div className="w-full flex items-center justify-center ">or</div>
+      <div className="flex items-center w-full gap-x-2">
+        <Button
+          size="lg"
+          className="w-full"
+          variant="outline"
+          onClick={() => {}}
+        >
+          <FcGoogle className="h-5 w-5" />
+        </Button>
+        <Button
+          size="lg"
+          className="w-full"
+          variant="outline"
+          onClick={() => {}}
+        >
+          <FaGithub className="h-5 w-5" />
+        </Button>
+      </div>
     </div>
   );
 };
